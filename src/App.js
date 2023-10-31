e05ddf67b897e33458f166788ecea06826b7fe9e
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-31 14:23:12 +0300

    example-add

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Axios from "axios"
 
 
-function App() {
-    const [catFact, setCatFact] = useState("")
+// function App() {
+//     const [catFact, setCatFact] = useState("")
 
-    const fetchCatFact = () => {
-        Axios.get("https://catfact.ninja/fact").then((res) => {
-            setCatFact(res.data.fact);
+//     const fetchCatFact = () => {
+//         Axios.get("https://catfact.ninja/fact").then((res) => {
+//             setCatFact(res.data.fact);
+//         })
+//     }
+
+//     useEffect(() => {
+//         fetchCatFact()
+//     }, [])
+
+
+//     return (
+//         <div className=''>
+//             <div className='d-flex justify-content-center align-items-center mt-5'>
+//                 <button className='btn btn-primary' onClick={fetchCatFact}>Generate Cat Fact</button>
+//             </div>
+
+//             <div className='d-flex justify-content-center align-items-center mt-5 fw-bold'>
+
+//                 <p>{catFact} </p>
+//             </div>
+//         </div>
+//     )
+// }
+function App() {
+    const [generatedExcuse, setGeneratedExcuse] = useState("")
+    const fetchExcuse = (excuse) => {
+        Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then((res) => {
+            setGeneratedExcuse(res.data[0].excuse)
         })
     }
 
-    useEffect(() => {
-        fetchCatFact()
-    }, [])
-
-
     return (
-        <div className=''>
-            <div className='d-flex justify-content-center align-items-center mt-5'>
-                <button className='btn btn-primary' onClick={fetchCatFact}>Generate Cat Fact</button>
-            </div>
-
-            <div className='d-flex justify-content-center align-items-center mt-5 fw-bold'>
-
-                <p>{catFact} </p>
-            </div>
+        <div className='App'>
+            <h1>Generate An Excuse</h1>
+            <button onClick={() => fetchExcuse("party")}>Party</button>
+            <button onClick={() => fetchExcuse("family")}>Family</button>
+            <button onClick={() => fetchExcuse("office")}>Office</button>
+            <p>{generatedExcuse}</p>
         </div>
     )
 }
-
 export default App
--- src/Example.js
+++ src/Example.js
@@ -1,32 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import Axios from "axios"
+// // import React, { useEffect, useState } from 'react'
+// // import Axios from "axios"
+
+// import { Axios } from "axios"
 
 
-function Example() {
-    const [name, setName] = useState()
-    const [predictedAge, setPredictedAge] = useState(null)
-    const fetchData = () => {
-        Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-            setPredictedAge(res.data)
-        })
-    }
+// // function Example() {
+// //     const [name, setName] = useState()
+// //     const [predictedAge, setPredictedAge] = useState(null)
+// //     const fetchData = () => {
+// //         Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+// //             setPredictedAge(res.data)
+// //         })
+// //     }
 
-    return (
-        <div>
-            <div className='d-flex justify-content-center align-items-center  mt-5'>
-                <input placeholder='Ex. Reso...' onChange={(event) => {
-                    setName(event.target.value)
-                }} />
-            </div>
+// //     return (
+// //         <div>
+// //             <div className='d-flex justify-content-center align-items-center  mt-5'>
+// //                 <input placeholder='Ex. Reso...' onChange={(event) => {
+// //                     setName(event.target.value)
+// //                 }} />
+// //             </div>
 
-            <div className='d-flex justify-content-center align-items-center mt-5'>
-                <button className='btn btn-primary' onClick={fetchData}>Predict Age</button>
-            </div>
-            <h1 className='d-flex justify-content-center align-items-center mt-5'>Name :{predictedAge?.name}</h1>
-            <h2 className='d-flex justify-content-center align-items-center mt-5'>Predicted Age:{predictedAge?.age}</h2>
-            <h2 className='d-flex justify-content-center align-items-center mt-5'>Count :{predictedAge?.count}</h2>
-        </div>
-    )
-}
+// //             <div className='d-flex justify-content-center align-items-center mt-5'>
+// //                 <button className='btn btn-primary' onClick={fetchData}>Predict Age</button>
+// //             </div>
+// //             <h1 className='d-flex justify-content-center align-items-center mt-5'>Name :{predictedAge?.name}</h1>
+// //             <h2 className='d-flex justify-content-center align-items-center mt-5'>Predicted Age:{predictedAge?.age}</h2>
+// //             <h2 className='d-flex justify-content-center align-items-center mt-5'>Count :{predictedAge?.count}</h2>
+// //         </div>
+// //     )
+// // }
 
-export default Example
+// function Example() {
+//     const fetchData = () => {
+//         Axios.get(`https://excuser.herokuapp.com/v1/excuse/party/`).thent(((res) => {
+//                        setPredictedAge(res.data)
+//                     })
+//               }
+
+   
+//     return (
+//         <div className='App'>
+//             <h1>Generate An Excuse</h1>
+//             <button></button>
+//         </div>
+//     )
+// }
+
+// export default Example
